7990c5df268bec10a261a8481a964feadeb74844
jeromeguillaume <jerome.guillaume@live.fr>
2023-01-06 11:32:25 +0100

    My 1st commit

---
+++ README.md
@@ -0,0 +1,237 @@
+# Kong plugin: XML Request Handling
+This Kong plugin is developed in Python and uses the Python library [lxml.etree](https://pypi.org/project/lxml/)  binding for the GNOME C libraries [libxml2](https://gitlab.gnome.org/GNOME/libxml2#libxml2) and [libxslt](https://gitlab.gnome.org/GNOME/libxslt#libxslt).
+
+The plugin handles the XML Request (in the HTTP body) in this order:
+1) Transform the XML with XSLT (XSLTransformation)
+2) Replace the values of XPath entries
+3) Validate XML against its XSD schema
+
+In case of misconfiguration the Plugin sends to the consumer an HTTP 400 error Bad Request (with the error detailed message)
+
+## How deploy XML Request Handling plugin
+1) Build a new Docker 'Kong Gateway' image with Python dependencies and the plugin code
+
+```
+docker build -t kong-gateway-xml-request-handling .
+```
+
+2) Create and prepare a PostgreDB called ```kong-database-xml-request-handling```.
+[See documentation](https://docs.konghq.com/gateway/latest/install/docker/#prepare-the-database).
+
+3) Start the Kong Gateway
+```
+./start-kong.sh
+```
+
+## How test XML Request Handling plugin with ```calcWebService/Calc.asmx```
+### Create a Kong Service and route
+1) Create a Kong Service named ```calcWebService``` with this URL: https://ecs.syr.edu/faculty/fawcett/Handouts/cse775/code/calcWebService/Calc.asmx.
+This simple backend Web Service adds 2 numbers.
+
+2) Create a Route on the Service ```calcWebService``` with the ```path``` value ```/calcWebService```
+
+3) Install the ```xml-hanlding``` plugin on the Service ```calcWebService``` and use the default parameters. The ```XsdSoapSchema``` property checks the entire SOAP XML content against its schema. The default value is defined by W3C, it can be found here: https://schemas.xmlsoap.org/soap/envelope/
+
+4) Call the ```calcWebService``` through the Kong Gateway Route by using [httpie](https://httpie.io/) tool
+```
+http POST http://localhost:8000/calcWebService \
+Content-Type:"text/xml; charset=utf-8" \
+SOAPAction: "http://tempuri.org/Add" \
+--raw "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"
+xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">
+  <soap:Body>
+    <Add xmlns=\"http://tempuri.org/\">
+      <a>5</a>
+      <b>7</b>
+    </Add>
+  </soap:Body>
+</soap:Envelope>"
+```
+
+The expected result is ```12```:
+```xml
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ...>
+  <soap:Body>
+    <AddResponse xmlns="http://tempuri.org/">
+      <AddResult>12</AddResult>
+    </AddResponse>
+  </soap:Body>
+</soap:Envelope>
+```
+### Example #1: adding a Tag in ```calcWebService``` XML request by using XSLT 
+The plugin applies a XSLTransformation on XML request.
+In this example the XSLT **adds the value ```<b>8</b>```** that will be not present in the request.
+Configure the plugin with:
+- ```XsltReplace``` property with this XSLT definition:
+```xml
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output omit-xml-declaration="yes" indent="yes"/>
+    <xsl:strip-space elements="*"/>
+    <xsl:template match="node()|@*">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*"/>
+        </xsl:copy>
+    </xsl:template>   
+    <xsl:template match="//*[local-name()='a']">
+       <xsl:copy-of select="."/>
+       <b>8</b>
+   </xsl:template>
+</xsl:stylesheet>
+```
+Use request defined at step #4, remove ```<b>7</b>```, the expected result is no longer ```12``` but ```13```:
+```xml
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ...>
+  <soap:Body>
+    <AddResponse xmlns="http://tempuri.org/">
+      <AddResult>13</AddResult>
+    </AddResponse>
+  </soap:Body>
+</soap:Envelope>
+```
+### Example #2: renaming a Tag in ```calcWebService``` XML request by using XSLT
+The plugin applies a XSLTransformation on XML request.
+In this example we **change the Tag name from ```<Subtract>...</Subtract>```** (present in the request) **to ```<Add>...</Add>```**.
+
+**Without XSLT**: Use request defined at step #4, rename the Tag ```<Add>...</Add>```, to ```<Subtract>...</Subtract>``` the expected result is ```-2```
+```xml
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ...>
+  <soap:Body>
+    <SubtractResponse xmlns="http://tempuri.org/">
+      <SubtractResult>-2</SubtractResult>
+    </SubtractResponse>
+  </soap:Body>
+</soap:Envelope>
+```
+
+Configure the plugin with:
+- ```XsltReplace``` property with this XSLT definition:
+```xml
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:template match="@*|node()">
+        <xsl:copy>
+            <xsl:apply-templates select="@*|node()" />
+        </xsl:copy>
+    </xsl:template>
+    <xsl:template match="//*[local-name()='Subtract']">
+       <Add xmlns="http://tempuri.org/"><xsl:apply-templates select="@*|node()" /></Add>
+   </xsl:template>
+</xsl:stylesheet>
+```
+**With XSLT**: Use request defined at step #4, rename the Tag ```<Add>...</Add>```, to ```<Subtract>...</Subtract>``` the expected result is ```12```:
+```xml
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ...>
+  <soap:Body>
+    <AddResponse xmlns="http://tempuri.org/">
+      <AddResult>13</AddResult>
+    </AddResponse>
+  </soap:Body>
+</soap:Envelope>
+```
+
+### Example #3: replacing the values of XPath entries in ```calcWebService``` XML request
+The plugin searches the 1st XPath entry and replaces the value with new one. In case of we want to change all XPath entries, just enable the ```XPathReplaceAll``` checkbox of the plugin property.
+
+In this example we replace the value of:
+- ```<a>```: former: ```5``` to new: ```10```
+- ```<b>```: former: ```7``` to new: ```9```
+
+Configure the plugin with:
+- ```XsltReplace``` property with no value
+- ```XPathReplace``` property with the value 
+```.//{http://tempuri.org/}a, .//{http://tempuri.org/}b```
+- ```XPathReplaceValue``` property with the value 
+```10,9```
+
+Use request defined at step #4, the expected result is no longer ```12``` but ```19```:
+```xml
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" ...>
+  <soap:Body>
+    <AddResponse xmlns="http://tempuri.org/">
+      <AddResult>19</AddResult>
+    </AddResponse>
+  </soap:Body>
+</soap:Envelope>
+```
+
+### Example #4: calling incorrectly ```calcWebService``` and detecting issue with XSD schema
+We call incorrectly the Service by injecting a SOAP error; the plugin detects it, sends an error message to the Consumer and Kong doesn't call the SOAP backend API.
+
+Configure the plugin with:
+- ```XsltReplace``` property with no value
+- ```XPathReplace``` property with no value
+- ```XPathReplaceValue``` property with no value
+- ```XsdSoapSchema``` property with the default value (defined by W3C) which checks the entire SOAP XML content against its schema 
+
+Use request defined at step #4, **change ```</soap:Envelope>``` by ```</soap:EnvelopeKong>```** => Kong says: 
+```json
+"XSD validation failed": "line 10: b'Opening and ending tag mismatch: Envelope line 2 and EnvelopeKong' (line 10)"
+```
+
+Configure the plugin with:
+- ```XsdApiSchema``` property with this value:
+```xml
+<xs:schema attributeFormDefault="unqualified" elementFormDefault="qualified" targetNamespace="http://tempuri.org/" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <xs:element name="Add" type="tem:AddType" xmlns:tem="http://tempuri.org/"/>
+  <xs:complexType name="AddType">
+    <xs:sequence>
+      <xs:element type="xs:integer" name="a" minOccurs="1"/>
+      <xs:element type="xs:integer" name="b" minOccurs="1"/>
+    </xs:sequence>
+  </xs:complexType>
+</xs:schema>
+```
+Use request defined at step #4, **remove ```<a>5</a>```** => there is an error because the ```<a>``` tag has the ```minOccurs="1"``` XSD property and Kong says: 
+```json
+"XSD validation failed": "Element '{http://tempuri.org/}b': This element is not expected. Expected is ( {http://tempuri.org/}a ). (<string>, line 0)"
+```
+Use request defined at step #4, **replace ```<b>7</b>``` by ```<b>ABCD</b>```** => there is an error because the ```<b>``` tag has the ```type="xs:integer"``` XSD property and Kong says: 
+ ```json
+ "XSD validation failed": "Element '{http://tempuri.org/}b': 'ABCD' is not a valid value of the atomic type 'xs:integer'. (<string>, line 0)"
+```
+
+### Example #5: chaining all plugin capabilities on ```calcWebService``` call
+Configure the plugin with:
+
+- ```XsltReplace``` property with no value
+- ```XPathReplace``` property with no value
+- ```XPathReplaceValue``` property with no value
+- ```XsdApiSchema``` property with the value defined at example #4
+- ```XsdSoapSchema``` property with the default value
+
+1) XSD schema Validation
+
+Use request defined at step #4, **remove ```<b>7</b>```** => there is an error because ```<b>``` is not present
+
+2) XSLTransformation + XSD schema Validation
+
+Configure the plugin with:
+- ```XsltReplace``` property with this XSLT definition which **adds the value ```<b>ABCD</b>```** that will be not present in the request but injected with a String value (instead of Integer)
+```xml
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output omit-xml-declaration="yes" indent="yes"/>
+    <xsl:strip-space elements="*"/>
+    <xsl:template match="node()|@*">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*"/>
+        </xsl:copy>
+    </xsl:template>   
+    <xsl:template match="//*[local-name()='a']">
+       <xsl:copy-of select="."/>
+       <b>ABCD</b>
+   </xsl:template>
+</xsl:stylesheet>
+```
+Use request defined at step #4, **remove ```<b>7</b>```** => there is an error because ```<b>``` is present but with a String value instead of Integer
+
+3) XSLTransformation + XPath replacement + XSD schema Validation
+- ```XPathReplace``` property with the value ```.//{http://tempuri.org/}b```
+- ```XPathReplaceValue``` property with the value 
+```7```
+
+Use request defined at step #4, **remove ```<b>7</b>```** => there is no more error because: 
+- XSLTransformation adds the ```<b>ABCD</b>```
+- Plugin replaces the value of ```<b>``` XPath entry: ```ABCD``` replaced by ```7```
+- The SOAP request is validated successfully against its XSD schema
+
+So, the backend Web Service is successfully called and the result is ```12```
---
+++ plugins/xml-request-handling.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python3
+import os
+import kong_pdk.pdk.kong as kong
+from lxml import etree
+from io import BytesIO
+
+import sys
+sys.path.append("/usr/local/kong/python/lib")
+
+import xsdSoapDefinition
+
+Schema = (
+    { "xsdSoapSchema": {"type": "string", "default": xsdSoapDefinition.XSD_SCHEMA_SOAP, "required": False} },
+    { "xsdApiSchema": {"type": "string", "required": False} },
+    { "XPathReplace": {"type": "string", "required": False} },
+    { "XPathReplaceAll": {"type": "boolean", "required": False} },
+    { "XPathReplaceValue": {"type": "string", "required": False} },
+    { "XSLTReplace": {"type": "string", "required": False} },
+)
+
+version = '1.1.0'
+priority = 10
+
+class XMLHandlingRequest:
+    def __init__(self, config):
+        self.config = config
+
+    #-------------------------------------
+    # Return a SOAP Fault to the Consumer
+    #-------------------------------------
+    def ReturnSOAPFault(self, kong, ErrMsg, ErrEx):
+        msgError = """<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+        <soap:Body>
+            <soap:Fault>
+                <faultcode>soap:Client</faultcode>
+                    <faultstring>{}: {}</faultstring>
+                    <detail/>
+            </soap:Fault>
+        </soap:Body>
+        </soap:Envelope>""".format(ErrMsg, ErrEx)
+        dictType = {"Content-Type": "text/xml; charset=utf-8"}
+        # Don't call the backend API and send back to Consumer a SOAP Error Message
+        return kong.response.exit(500, msgError, dictType)
+        
+    #-------------------------------------
+    # Transform the XML content with XSLT
+    #-------------------------------------
+    def XSLTransform (self, kong):
+        kong.log.notice("XSLTransform *** BEGIN ***")
+        XSLTReplace = ""
+        try:
+            if 'XSLTReplace' in self.config:
+                XSLTReplace = self.config['XSLTReplace']
+        except:
+            return
+
+        # If we don't have the same number of parameters of 'XPathReplace' and 'XPathReplaceValue' we return an Error
+        if XSLTReplace == "":
+            kong.log.notice("No XSLT transformation is configured, so there is nothing to do")
+            return
+
+        # Get XML SOAP envelope from consumer's Request
+        soapEnvelope = ""
+        try:
+            soapEnvelope = kong.request.get_raw_body()
+        except Exception as ex:
+            kong.log.err("Unable to get raw body from request, exception={}".format(ex))
+            # Return a SOAP Fault to the Consumer
+            self.ReturnSOAPFault(kong, "XSLT Transformation failed", ex)
+            
+        kong.log.notice("BEFORE XSLT transform, body= ", soapEnvelope)
+        try:
+            
+            # Construct the XSLT transformer
+            xslt_root = etree.XML(XSLTReplace)
+            transform = etree.XSLT(xslt_root)
+
+            # Run the transformation on the XML SOAP envelope
+            doc = etree.parse(BytesIO(soapEnvelope))
+            result_tree = transform(doc)
+            
+            # Remove empty Namepace added by the 'lxml' library (xmlns="")
+            result_tree_no_empty_xmlns = etree.tostring(result_tree).replace(b' xmlns=""', b'')
+            
+            # Change the Consumer request with the new XST transformed values
+            kong.log.notice("AFTER XSLT transform, body=", result_tree_no_empty_xmlns)
+            kong.service.request.set_raw_body (result_tree_no_empty_xmlns)
+
+        except Exception as ex:
+            kong.log.err("XSLT Transformation, exception={}".format(ex))
+            # Return a SOAP Fault to the Consumer
+            self.ReturnSOAPFault(kong, "XSLT Transformation failed", ex)
+        
+        kong.log.notice("XSLTransform *** END ***")
+    
+    #---------------------------------
+    # Replace the value of XPath tags
+    #---------------------------------
+    def XPathReplace (self, kong):
+        kong.log.notice("XPathReplace *** BEGIN ***")
+
+        # Get XPath to be replaced and Value
+        XPathReplace = ""
+        XPathReplaceAll = False
+        XPathReplaceValue = ""
+        try:
+            if 'XPathReplace' in self.config:
+                XPathReplace = self.config['XPathReplace']
+            if 'XPathReplaceAll' in self.config:
+                XPathReplaceAll = self.config['XPathReplaceAll']
+            if 'XPathReplaceValue' in self.config:
+                XPathReplaceValue = self.config['XPathReplaceValue']
+        except:
+            return
+        
+        kong.log.notice("XPathReplace={} | XPathReplaceAll={} | XPathReplaceValue={}".format(XPathReplace, XPathReplaceAll, XPathReplaceValue))
+        
+        # If there is no replacement, we exit
+        if XPathReplace == "" or XPathReplaceValue == "":
+            kong.log.notice("No XPATH replacement is configured, so there is nothing to do")
+            return
+        
+        # If we don't have the same number of parameters of 'XPathReplace' and 'XPathReplaceValue' we return an Error
+        if len(XPathReplace.split(",")) != len(XPathReplaceValue.split(",")):
+            msgError = "The number of entries in 'XPathReplace' and 'XPathReplaceValue' are different"
+            kong.log.err(msgError)
+            # Return a SOAP Fault to the Consumer
+            self.ReturnSOAPFault(kong, "XPath replacement failed", msgError)
+            
+        soapEnvelope = ""
+        try:
+            soapEnvelope = kong.request.get_raw_body()
+        except Exception as ex:
+            kong.log.err("Unable to get raw body from request, exception={}".format(ex))
+            # Return a SOAP Fault to the Consumer
+            self.ReturnSOAPFault(kong, "XPath replacement failed", ex)
+        
+        kong.log.notice("BEFORE XPath replacement, body= ", soapEnvelope)
+        
+        try:
+            treeSOAP = etree.parse(BytesIO(soapEnvelope))
+            rootSOAP = treeSOAP.getroot()
+            # Create a list of all XPath entries and values to be replaced
+            XPathReplace_list      = XPathReplace.split(",")
+            XPathReplaceValue_list = XPathReplaceValue.split(",")
+            v = 0
+
+            # Iterate on each XPath entry to be replaced
+            for XPathReplace_nth in XPathReplace_list:
+                # Get matched number of XPath entries
+                xpathNb = len(rootSOAP.findall(XPathReplace_nth.strip()))
+
+                # If all XPath matched entries are replaced
+                if XPathReplaceAll == True:
+                    # Replace all XPath entries
+                    for i in range(xpathNb):
+                        rootSOAP.findall(XPathReplace_nth.strip())[i].text = XPathReplaceValue_list[v].strip()
+                # Only 1st XPath match is replaced
+                else:
+                    rootSOAP.find(XPathReplace_nth.strip()).text = XPathReplaceValue_list[v].strip()
+                # Increment the index for the Values list
+                v += 1
+            # Change the Consumer request with the new XPath values
+            kong.service.request.set_raw_body (etree.tostring(rootSOAP))
+            kong.log.notice("AFTER XPath replacement, body=", etree.tostring(rootSOAP))
+
+        except Exception as ex:
+            kong.log.err("Replace the value of XPath tags, exception={}".format(ex))
+            msgError = "{}".format(ex)
+            # Return a SOAP Fault to the Consumer
+            self.ReturnSOAPFault(kong, "XPath replacement failed", ex)
+        
+        kong.log.notice("XPathReplace *** END ***")
+
+    #--------------------------------------------------------------------
+    # Validate XML against XSD schema
+    # SOAP schema and API schema are defined in the configuration plugin
+    #--------------------------------------------------------------------
+    def XMLValidate (self, kong):
+        kong.log.notice("XMLValidate *** BEGIN ***")
+        soapEnvelope = ""
+        tree = ""
+        
+        try:
+            soapEnvelope = kong.request.get_raw_body()
+        except Exception as ex:
+            kong.log.err("Unable to get raw body from request, exception=", "%s" % (ex))
+            return
+        
+        kong.log.notice("XMLValidate body= ", soapEnvelope)
+        
+        # Get XSD SOAP shema from the plugin configuration
+        xsdSoapSchema = ""
+        try:
+            if 'xsdSoapSchema' in self.config:
+                xsdSoapSchema = self.config['xsdSoapSchema']
+        except:
+            pass
+        
+        # If we have a Body content, we check the validity of SOAP envelope against its XSD schema
+        if soapEnvelope != "" and xsdSoapSchema != "":
+            try:
+                kong.log.notice("Check SOAP Envelope XML against its XSD schema")
+
+                # Load the SOAP envelope XSD schema
+                schema_root_soap = etree.XML (xsdSoapSchema)
+                schema_root_soap_etree = etree.XMLSchema(schema_root_soap)
+                parse_root_soap = etree.XMLParser(schema = schema_root_soap_etree)
+                
+                # Parse the SOAP envelope (retrieved from request) against the schema
+                tree = etree.parse(BytesIO(soapEnvelope), parse_root_soap)
+            except Exception as ex:
+                kong.log.err("Unable check the validity of SOAP envelope against its XSD schema, exception=", "%s" % (ex))
+                # Return a SOAP Fault to the Consumer
+                self.ReturnSOAPFault(kong, "XSD validation failed", ex)
+    
+        # Get XSD API shema from the plugin configuration
+        xsdApiSchema = ""
+        try:
+            if 'xsdApiSchema' in self.config:
+                xsdApiSchema = self.config['xsdApiSchema']
+        except:
+            pass
+
+        # If we have a Body content, we check the validity of <soap:Body> content against its XSD schema
+        if soapEnvelope != "" and xsdApiSchema != "":
+            try:
+                kong.log.notice("Check API XML against its XSD schema")
+                if tree == "":
+                    tree = etree.parse(BytesIO(soapEnvelope))
+
+                # Load the API XSD schema
+                schema_api = etree.XML(xsdApiSchema)
+                schema_api_etree = etree.XMLSchema(schema_api)
+                parser_api = etree.XMLParser(schema = schema_api_etree)
+
+                # Get <soap:Body> content from entire <soap:Envelope>
+                #
+                # Example:
+                # <soap:Envelope xmlns:xsi=....">
+                #   <soap:Body>
+                #     <Add xmlns="http://tempuri.org/">
+                #       <a>5</a>
+                #       <b>7</b>
+                #     </Add>
+                #   </soap:Body>
+                # </soap:Envelope>
+                root = tree.getroot()
+
+                # root.getchildren()[0] => <soap:Body>...</soap:Body>
+                # root.getchildren()[0].getchildren()[0] => <Add ...</Add>
+                bodySOAPContent = root.getchildren()[0].getchildren()[0]
+                kong.log.notice("etree.tostring(bodySOAPContent=", etree.tostring(bodySOAPContent))
+                
+                # Parse the SOAP envelope (retrieved from request) agains the schema
+                tree = etree.parse(BytesIO(etree.tostring(bodySOAPContent)), parser_api)
+            except Exception as ex:
+                kong.log.err("Unable check the validity of API content against its XSD schema, exception=", "%s" % (ex))
+                # Return a SOAP Fault to the Consumer
+                self.ReturnSOAPFault(kong, "XSD validation failed", ex)
+
+        kong.log.notice("XMLValidate *** END ***")
+
+#--------------------------------------------------------------------
+# This plugin handles XML content in this order:
+# 1) Transform the XML content with XSLT
+# 2) Replace the value of XPath tags
+# 3) Check XML validity against the XSD schema (XSD SOAP and XSD API)
+#--------------------------------------------------------------------
+class Plugin(object):
+    def __init__(self, config):
+        self.config = config
+
+    #----------------------------------------------------
+    # Executed for every request from a client and 
+    # before it is being proxied to the upstream service
+    #----------------------------------------------------
+    def access(self, kong: kong.kong):
+        kong.log.notice("access *** BEGIN ***")
+        
+        try:
+            xmlHReq = XMLHandlingRequest(self.config)
+            
+            # Transform XML with XSLT Transformation
+            xmlHReq.XSLTransform(kong)
+
+            # Replace the value of XPath tags
+            xmlHReq.XPathReplace(kong)
+
+            # Validate XML against XSD schema
+            xmlHReq.XMLValidate(kong)
+
+        except Exception as ex:
+            kong.log.err("XML Handling error, exception= {}".format(ex))
+            return
+
+        kong.log.notice("access *** END ***")
+
+# add below section to allow this plugin optionally be running in a dedicated process
+if __name__ == "__main__":
+    from kong_pdk.cli import start_dedicated_server
+    start_dedicated_server("xml-request-handling", Plugin, version, priority, Schema)
